fd1e2765dba6b7275996469ddce6bde51e068778
HYUNSOOSHIN <shs0655@naver.com>
2018-11-30 01:25:21 +0900

    posts 라우터 뜯어고침
    1. posting , photo 경로 합쳐놨음
    = http://ip:3000/posts/photo경로를 통하면 됨.
    2. 사용자이름 받아오는거랑 포스트 db에 저장하는거 새로 고침.

--- webapp/server/routes/posts.js
+++ webapp/server/routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Post = require('./../models/post')
 var mongoose = require('mongoose');
-var localuser;
+var User = require('./../models/user')
 
 mongoose.connect('mongodb://localhost:27017/member', {useNewUrlParser: true});
 
@@ -11,28 +11,38 @@ router.get('/', function(req, res, next) {
     res.render('post');
 });
 
-router.get('/posting', ensureAuthorized, function(req, res, next) {
-    localuser = req.username;
-    console.log(localuser)
-});
-
-router.post('/photo',  function (req, res, next) {
+router.post('/photo', ensureAuthorized, function (req, res, next) {
     //res.render('posting');
     //res.send({success:true, data:savedPost})
     //localuser = req.username;
-    console.log(localuser)
-    
-    var post = new Post();
-    post.text = req.body.text;
-    post.username = localuser;
-    console.log(req.body)
-    Post.create(post, function() {res.send(post)});
-    //localPosting(post, 
-    //   function(err, savedPost){
-    //        res.send({success:true, data:savedPost})
-    //    }, 
-    // );
-    
+    var findConditionToken ={
+        jsonWebToken: req.token
+      };
+
+    User.findOne(findConditionToken, function(err, user){
+        if(err) {res.send({success:false, type:"Error Occured"+err});}
+        else {
+            var username = user.username
+            console.log("username: "+username)
+            
+            var post = new Post();
+            post.text = req.body.text;
+            post.username = username;
+            console.log(req.body)
+            
+            localPosting(post, function(err, savedPost){
+                if (err){
+                  res.send({success:false, data:"Error Occured"+err});
+                } else {
+                  res.send({
+                    success: true,
+                    data: savedPost,
+                    type: "post",
+                  });
+                }
+              });
+        }
+      })
 });
 
 
@@ -47,11 +57,10 @@ function localPosting(Post, next) {
 function ensureAuthorized(req, res, next) {
     var bearerToken;
     var bearerHeader = req.headers["authorization"];
-//    console.log(bearerHeader);
     if (typeof bearerHeader !== "undefined") {
       var bearer = bearerHeader.split(" "); 
       bearerToken = bearer[0];
-      req.username = bearerToken;
+      req.token = bearerToken;
       next();
     } else{
       res.sendStatus(403);
